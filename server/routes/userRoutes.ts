a80b4f48f2744b2187c5bd712423468a4309f6fa
nemanjamalesija <malesija13@gmail.com>
2023-04-21 15:43:43 +0200

    Add user controller and router

---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import userController from '../controllers/userController.ts';
+
+const router = express.Router();
+
+router.route('/').get(userController.getAllUsers).post(userController.createUser);
+
+router
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
+
+export default router;
