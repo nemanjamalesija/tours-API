40903e1dcf8d380851191b35cdc9a6f46f5f1b97
nemanjamalesija <malesija13@gmail.com>
2023-04-23 23:55:17 +0200

    Add field limiting

--- server/controllers/tourController.ts
+++ server/controllers/tourController.ts
@@ -5,7 +5,8 @@ const getAllTours = async (req: Request, res: Response) => {
   try {
     console.log(req.query);
 
-    // BUILD QUERY
+    // 1 BUILDING QUERY
+
     // 1a Filtering
     const queryObject = { ...req.query };
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
@@ -17,13 +18,20 @@ const getAllTours = async (req: Request, res: Response) => {
 
     let query = Tour.find(JSON.parse(queryString));
 
-    // 1c Sorting
+    // 2 SORTING
     if (req.query.sort) {
       const sortBy = req.query.sort.toString().split(',').join(' ');
       console.log(sortBy);
 
       query = query.sort(sortBy);
     } else query = query.sort('price');
+
+    //3 FIELD LIMITING
+    if (req.query.fields) {
+      const fields = req.query.fields.toString().split(',').join(' ');
+
+      query = query.select(fields);
+    } else query = query.select('-__v');
 
     // EXECUTE QUERY
     const tours = await query;
