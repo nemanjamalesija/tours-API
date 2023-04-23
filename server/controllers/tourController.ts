04d4692c12fdcdc343036190cde4ca19edb184e0
nemanjamalesija <malesija13@gmail.com>
2023-04-24 00:13:12 +0200

    Add pagination

--- server/controllers/tourController.ts
+++ server/controllers/tourController.ts
@@ -33,6 +33,19 @@ const getAllTours = async (req: Request, res: Response) => {
       query = query.select(fields);
     } else query = query.select('-__v');
 
+    // PAGINATION
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page && ) {
+      const numTours = await Tour.countDocuments();
+
+      if (skip >= numTours) throw new Error('This page does not exist!');
+    }
+
     // EXECUTE QUERY
     const tours = await query;
 
