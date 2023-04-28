228930d5a9cf00ad9d0295705b2cebb7b7c6697c
nemanjamalesija <malesija13@gmail.com>
2023-04-29 01:30:35 +0200

    Fix sign up controller (security issue)

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -2,15 +2,22 @@ import { NextFunction, Request, Response } from 'express';
 import User from '../models/userModel.ts';
 import catchAsync from '../helpers/catchAsync.ts';
 
-const signUp = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-  const newUser = await User.create({ ...req.body });
+const signUp = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      passwordConfirm: req.body.passwordConfirm,
+    });
 
-  res.status(201).json({
-    status: 'success',
-    data: {
-      user: newUser,
-    },
-  });
-});
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: newUser,
+      },
+    });
+  }
+);
 
 export default { signUp };
