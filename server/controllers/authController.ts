b2a1315994506ea6b73d9d9d6345e8ecd4589d1c
nemanjamalesija <malesija13@gmail.com>
2023-04-29 01:49:22 +0200

    Generate json web token on sign up

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { NextFunction, Request, Response } from 'express';
 import User from '../models/userModel.ts';
 import catchAsync from '../helpers/catchAsync.ts';
@@ -11,8 +12,15 @@ const signUp = catchAsync(
       passwordConfirm: req.body.passwordConfirm,
     });
 
+    const token = jwt.sign(
+      { id: newUser._id },
+      process.env.JWT_SECRET_STRING as string,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
     res.status(201).json({
       status: 'success',
+      token,
       data: {
         user: newUser,
       },
