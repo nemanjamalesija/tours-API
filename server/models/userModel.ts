8892c0f2c4912808e23387c1dfe8cbca58f25d90
nemanjamalesija <malesija13@gmail.com>
2023-04-29 01:08:10 +0200

    Encrypt passwords

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,6 +1,7 @@
 import { userType } from '../types/modelsTypes.ts';
 import mongoose from 'mongoose';
 import validator from '../helpers/validator.ts';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema<userType>({
   name: {
@@ -29,7 +30,7 @@ const userSchema = new mongoose.Schema<userType>({
   },
 });
 
-// validate password (works only on create and save)
+// validate password (works only on CREATE and SAVE)
 userSchema.path('passwordConfirm').validate(function (value: string) {
   if (value !== this.get('password')) {
     throw new Error('Passwords are not the same!');
@@ -37,8 +38,15 @@ userSchema.path('passwordConfirm').validate(function (value: string) {
   return true;
 });
 
-userSchema.pre('save', function (next) {
+userSchema.pre('save', async function (next) {
+  // Only run if password was actually modified
   if (!this.isModified('password')) return next();
+
+  // Hash the password with the cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = '';
 });
 
 const User = mongoose.model<userType>('User', userSchema);
