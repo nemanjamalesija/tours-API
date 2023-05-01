4ccb6479534e6311d3db5595d10cbe4ea9d52a77
nemanjamalesija <malesija13@gmail.com>
2023-05-02 01:38:25 +0200

    Add user info update

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,6 +1,16 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import catchAsync from '../helpers/catchAsync.ts';
 import User from '../models/userModel.ts';
+import AppError from '../helpers/appError.ts';
+
+const filterObj = (obj: any, ...allowedFields: string[]) => {
+  const newObj: { [key: string]: string } = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 const getAllUsers = catchAsync(async (req: Request, res: Response) => {
   const users = await User.find();
@@ -11,6 +21,42 @@ const getAllUsers = catchAsync(async (req: Request, res: Response) => {
     data: { users },
   });
 });
+
+const updateMe = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    // 1. Create error if user posts password data
+    if (req.body.password || req.body.passwordConfirm) {
+      const error = new AppError(
+        'This route is not for password update. Please use /updateMypassword',
+        400
+      );
+
+      next(error);
+    }
+
+    // 2. Filter fields that are not allowed
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    // 3. Update user document
+    const updatedUser = await User.findByIdAndUpdate(
+      req.body.currentUser.id,
+      filteredBody,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+    console.log(updatedUser);
+    //4. Send response to the client
+    res.status(200).json({
+      status: 'sucess',
+      data: {
+        updatedUser,
+      },
+    });
+  }
+);
+
 const getUser = (req: Request, res: Response) => {
   res.status(500).json({
     status: 'error',
@@ -36,4 +82,11 @@ const deleteUser = (req: Request, res: Response) => {
   });
 };
 
-export default { getAllUsers, getUser, createUser, updateUser, deleteUser };
+export default {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  updateMe,
+};
