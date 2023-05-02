c0768f7053ec253ccdbc39b575f2f42a694d9dbe
nemanjamalesija <malesija13@gmail.com>
2023-05-02 11:07:42 +0200

    Add rate limiting

--- server/app.ts
+++ server/app.ts
@@ -10,12 +10,24 @@ import userRouter from './routes/userRoutes.ts';
 import AppError from './helpers/appError.ts';
 import { HttpError } from './types/errorType.ts';
 import globalErrorHandler from './controllers/errorController.ts';
+import rateLimit from 'express-rate-limit';
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 
 const app = express();
-app.use(morgan('dev'));
+
+// GLOBAL MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // miliseconds (1 hour)
+  message: 'Too many request from this IP. Please try again in an hour',
+});
+app.use('/api', limiter);
 app.use(express.json());
 app.use(cors());
 app.use(express.static(`${__dirName}/public`));
