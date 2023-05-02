e3642ccc180dcf9b11c47c46addd21f310c922e6
nemanjamalesija <malesija13@gmail.com>
2023-05-02 11:18:44 +0200

    Install dependecies

--- server/app.ts
+++ server/app.ts
@@ -11,6 +11,7 @@ import AppError from './helpers/appError.ts';
 import { HttpError } from './types/errorType.ts';
 import globalErrorHandler from './controllers/errorController.ts';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
@@ -18,18 +19,34 @@ const __dirName = path.dirname(__fileName);
 const app = express();
 
 // GLOBAL MIDDLEWARES
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit request from the same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000, // miliseconds (1 hour)
   message: 'Too many request from this IP. Please try again in an hour',
 });
 app.use('/api', limiter);
-app.use(express.json());
+
+// Cross origin policy
 app.use(cors());
+
+// Body parser
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+
+// Data sanitization against XSS
+
+// Serving static files
 app.use(express.static(`${__dirName}/public`));
 
 app.use('/api/v1/tours', tourRouter);
