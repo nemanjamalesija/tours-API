32245ba69e7502645ccb5a60cd8c87a0c150f924
nemanjamalesija <malesija13@gmail.com>
2023-05-05 06:42:32 +0200

    Complete review router

--- server/routes/reviewRoutes.ts
+++ server/routes/reviewRoutes.ts
@@ -4,11 +4,12 @@ import authController from '../controllers/authController.ts';
 
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect);
+
 router
   .route('/')
   .get(reviewController.getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     reviewController.setTourUserIds,
     reviewController.createReview
@@ -16,9 +17,17 @@ router
 
 router.route('/');
 router.route('/:id').get(reviewController.getSingleReview);
-
-router.route('/:id').patch(reviewController.updateReview);
-
-router.route('/:id').delete(reviewController.deleteReview);
+router
+  .route('/:id')
+  .patch(
+    authController.restrictTo('user, admin'),
+    reviewController.updateReview
+  );
+router
+  .route('/:id')
+  .delete(
+    authController.restrictTo('user, admin'),
+    reviewController.deleteReview
+  );
 
 export default router;
